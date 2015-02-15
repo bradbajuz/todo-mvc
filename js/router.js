5d675c6863c3f3b33c7b6cef3eb20b5df97b49c8
Brad Ballard <brad@bradballard.com>
2015-02-15 16:31:38 -0500

    Show only complete todos

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -17,9 +18,20 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 });
 
 Todos.TodosActiveRoute = Ember.Route.extend({
-  model: function(){
+  model: function() {
     return this.store.filter('todo', function(todo) {
       return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
     });
   },
   renderTemplate: function(controller) {
